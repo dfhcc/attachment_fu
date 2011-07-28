cc83a973ad8a4da736032f03c9ecb50596a2a2a0
Nico Ritsche <nico@ip-52-24.dhcp.opintanner.fi>
2011-07-29 00:22:09 +0300

    removed overwritten Tempfile tempname

--- lib/attachment_fu.rb
+++ lib/attachment_fu.rb
@@ -1,12 +1,12 @@
-require 'tempfile'
-
-Tempfile.class_eval do
-  # overwrite so tempfiles use the extension of the basename.  important for rmagick and image science
-  def make_tmpname(basename, n)
-    ext = nil
-    sprintf("%s%d-%d%s", basename.to_s.gsub(/\.\w+$/) { |s| ext = s; '' }, $$, (n || Time.now.tv_usec), ext)
-  end
-end
+#require 'tempfile'
+#
+#Tempfile.class_eval do
+#  # overwrite so tempfiles use the extension of the basename.  important for rmagick and image science
+#  def make_tmpname(basename, n)
+#    ext = nil
+#    sprintf("%s%d-%d%s", basename.to_s.gsub(/\.\w+$/) { |s| ext = s; '' }, $$, (n || Time.now.tv_usec), ext)
+#  end
+#end
 
 require 'geometry'
 require 'technoweenie/attachment_fu'
